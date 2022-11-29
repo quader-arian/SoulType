f4edbc3326e4c88348c94ed68a0dac5168363410
DangoX3 <nikolaschiu@gmail.com>
2022-11-29 18:02:21 -0500

    Added models and their animations
    added 8 out of 10 models
    gonna fix the last 2

---
+++ SoulType/Assets/Nik/Models/Ghoul/GhoulStateController.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhoulStateController : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = animator.GetBool("isWalking");
+        bool press1 = Input.GetKey("1");
+        bool press2 = Input.GetKey("2");
+        bool press3 = Input.GetKey("3");
+        bool press4 = Input.GetKey("4");
+
+        // if not walking and press 1
+        if (!isWalking && press1)
+        {
+            // start walking
+            animator.SetBool("isWalking", true);
+        }
+
+        // if walking and not pressing 1
+        if (isWalking && !press1)
+        {
+            // stop walking
+            animator.SetBool("isWalking", false);
+        }
+
+        if (press2)
+        {
+            animator.SetBool("isPreparing", true);
+        }
+
+        if (!press2)
+        {
+            animator.SetBool("isPreparing", false);
+        }
+
+        if (press3)
+        {
+            animator.SetBool("isAttacking", true);
+        }
+
+        if (!press3)
+        {
+            animator.SetBool("isAttacking", false);
+        }
+
+        if (press4)
+        {
+            animator.SetBool("isFlinching", true);
+        }
+
+        if (!press4)
+        {
+            animator.SetBool("isFlinching", false);
+        }
+    }
+}
