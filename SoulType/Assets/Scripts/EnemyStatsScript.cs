1e1b0205f827e63fcbae92f08be56e330299d014
quader-arian <arianq@my.yorku.ca>
2022-12-01 09:01:00 -0500

    sleepy - added pinkfoot to combat scene

--- SoulType/Assets/Scripts/EnemyStatsScript.cs
+++ SoulType/Assets/Scripts/EnemyStatsScript.cs
@@ -96,6 +96,8 @@ public class EnemyStatsScript : MonoBehaviour
             observer.SetActive(true);
         }else if(enemyType == "Emperor"){
             ghoul.SetActive(true);
+        }else if(enemyType == "Pinkfoot"){
+            pinkfoot.SetActive(true);
         }
     }
 
